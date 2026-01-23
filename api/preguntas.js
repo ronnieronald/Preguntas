d12e5f3b56b8a61d43959ae1be2b5cf05084abe7
ronnieronald <ronnieronald748@gmail.com>
2026-01-22 21:38:49 -0500

    update api/preguntas.js

--- api/preguntas.js
+++ api/preguntas.js
@@ -1,15 +1,15 @@
-import fetch from 'node-fetch'
-
 export default async function handler(req, res) {
   try {
     const githubUrl = 'https://raw.githubusercontent.com/ronnieronald/Preguntas/main/Preguntas.json'
 
+    // fetch nativo
     const response = await fetch(githubUrl, { cache: 'no-store' })
     if (!response.ok) throw new Error('No se pudo obtener el JSON de GitHub')
 
     const preguntas = await response.json()
     res.status(200).json(preguntas)
   } catch (err) {
+    console.error(err) // ayuda a depurar en Vercel logs
     res.status(500).json({ error: err.message })
   }
 }
