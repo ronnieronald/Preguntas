f0973b81381535331857b43e0e1ad37739142f12
ronnieronald <ronnieronald748@gmail.com>
2026-01-22 21:36:15 -0500

    update api/preguntas.js

--- api/preguntas.js
+++ api/preguntas.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 export default async function handler(req, res) {
   try {
-    const githubUrl = 'https://raw.githubusercontent.com/tuusuario/tu-repo/main/Preguntas.json'
+    const githubUrl = 'https://raw.githubusercontent.com/ronnieronald/Preguntas/main/Preguntas.json'
 
     const response = await fetch(githubUrl, { cache: 'no-store' })
     if (!response.ok) throw new Error('No se pudo obtener el JSON de GitHub')
